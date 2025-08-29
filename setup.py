c082a278afe978b416486215a59d47461c126a54
itskhateeb <rahmankhateeb14@gmail.com>
2025-08-29 19:53:22 +0530

    installed libraries

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_visa",
+    version="0.0.0",
+    author="Khateeb",
+    author_email="rahmankhateeb14@gmail.com",
+    packages=find_packages()
+)
